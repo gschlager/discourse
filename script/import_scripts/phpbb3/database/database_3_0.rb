b794ed6278d071dcfc5d726b2fd888a3bf6bd12c
Gerhard Schlager <mail@gerhard-schlager.at>
2015-08-22 23:18:59 +0200

    Cleanup the Sequel code a little bit

--- script/import_scripts/phpbb3/database/database_3_0.rb
+++ script/import_scripts/phpbb3/database/database_3_0.rb
@@ -2,6 +2,7 @@ require_relative 'database_base'
 require_relative '../support/constants'
 
 module ImportScripts::PhpBB3
+  # noinspection RubyResolve
   class Database_3_0 < DatabaseBase
     def count_users
       @database
@@ -13,6 +14,8 @@ module ImportScripts::PhpBB3
 
     def fetch_users(offset)
       unix_timestamp = Time.now.to_i
+      banlist_join_condition = Sequel.virtual_row { (u__user_id =~ b__ban_userid) & (b__ban_exclude =~ 0) &
+        ((b__ban_end =~ 0) | (b__ban_end >= unix_timestamp)) }
 
       @database
         .select(:u__user_id, :u__user_email, :u__username, :u__user_password, :u__user_regdate, :u__user_lastvisit,
@@ -21,8 +24,7 @@ module ImportScripts::PhpBB3
                 :u__user_avatar_type, :u__user_avatar)
         .from(table(:users, :u))
         .join(table(:groups, :g), [:group_id])
-        .left_outer_join(table(:banlist, :b), Sequel.virtual_row { (u__user_id =~ b__ban_userid) & (b__ban_exclude =~ 0) &
-                                              ((b__ban_end =~ 0) | (b__ban_end >= unix_timestamp)) })
+        .left_join(table(:banlist, :b), banlist_join_condition)
         .where { (u__user_type !~ Constants::USER_TYPE_IGNORE) }
         .order(:u__user_id)
         .limit(@batch_size)
@@ -50,17 +52,16 @@ module ImportScripts::PhpBB3
     end
 
     def fetch_categories
+      first_post_times = @database
+                           .from(table(:topics))
+                           .select_group(:forum_id)
+                           .select_append { min(topic_time).as(first_post_time) }
+                           .as(:x)
+
       @database
         .select(:f__forum_id, :f__parent_id, :f__forum_name, :f__forum_desc, :x__first_post_time)
         .from(table(:forums, :f))
-        .left_outer_join(
-          @database
-            .from(table(:topics))
-            .select_group(:forum_id)
-            .select_append { min(topic_time).as(first_post_time) }
-            .as(:x),
-          [:forum_id]
-        )
+        .left_join(first_post_times, [:forum_id])
         .where { (f__forum_type !~ Constants::FORUM_TYPE_LINK) }
         .order(:f__parent_id, :f__left_id)
         .all
@@ -73,11 +74,13 @@ module ImportScripts::PhpBB3
     end
 
     def fetch_posts(offset)
+      # CASE t.poll_length WHEN 0 THEN NULL ELSE t.poll_start + t.poll_length END AS poll_end
+      poll_end = Sequel.case({0 => nil}, Sequel.+(:t__poll_start, :t__poll_length), :t__poll_length).as(:poll_end)
+
       @database
         .select(:p__post_id, :p__topic_id, :t__forum_id, :t__topic_title, :t__topic_first_post_id, :p__poster_id,
                 :p__post_text, :p__post_time, :p__post_username, :t__topic_status, :t__topic_type, :t__poll_title,
-                Sequel.case({0 => nil}, Sequel.+(:t__poll_start, :t__poll_length), :t__poll_length).as(:poll_end),
-                :t__poll_max_options, :p__post_attachment)
+                poll_end, :t__poll_max_options, :p__post_attachment)
         .from(table(:posts, :p))
         .join(table(:topics, :t), [:topic_id])
         .order(:p__post_id)
@@ -161,14 +164,17 @@ module ImportScripts::PhpBB3
                             .from(table(:attachments))
                             .group_and_count(:post_msg_id)
                             .where(:topic_id => 0)
+                            .as(:a)
+
+      attachment_count = Sequel.function(:coalesce, :a__count, 0).as(:attachment_count)
 
       if use_fixed_messages
         @database
           .select(:m__msg_id, :i__root_msg_id, :m__author_id, :m__message_time, :m__message_subject,
-                  :m__message_text, Sequel.function(:coalesce, :a__count, 0).as(:attachment_count))
+                  :m__message_text, attachment_count)
           .from(table(:privmsgs, :m))
           .join(table(:import_privmsgs, :i), :m__msg_id => :i__msg_id)
-          .left_outer_join(attachment_counts, {:m__msg_id => :a__post_msg_id}, :table_alias => :a)
+          .left_join(attachment_counts, {:m__msg_id => :a__post_msg_id})
           .order(:i__root_msg_id, :m__msg_id)
           .limit(@batch_size)
           .offset(offset)
@@ -176,9 +182,9 @@ module ImportScripts::PhpBB3
       else
         @database
           .select(:m__msg_id, :m__root_level___root_msg_id, :m__author_id, :m__message_time, :m__message_subject,
-                  :m__message_text, Sequel.function(:coalesce, :a__count, 0).as(:attachment_count))
+                  :m__message_text, attachment_count)
           .from(table(:privmsgs, :m))
-          .left_outer_join(attachment_counts, {:m__msg_id => :a__post_msg_id}, :table_alias => :a)
+          .left_join(attachment_counts, {:m__msg_id => :a__post_msg_id})
           .order(:m__root_level, :m__msg_id)
           .limit(@batch_size)
           .offset(offset)
@@ -234,12 +240,12 @@ module ImportScripts::PhpBB3
 
     def get_config_values
       @database.select(
-        (@database.select(:config_value).from(table(:config)).where(:config_name => 'version')).as(:phpbb_version),
-        (@database.select(:config_value).from(table(:config)).where(:config_name => 'avatar_gallery_path')).as(:avatar_gallery_path),
-        (@database.select(:config_value).from(table(:config)).where(:config_name => 'avatar_path')).as(:avatar_path),
-        (@database.select(:config_value).from(table(:config)).where(:config_name => 'avatar_salt')).as(:avatar_salt),
-        (@database.select(:config_value).from(table(:config)).where(:config_name => 'smilies_path')).as(:smilies_path),
-        (@database.select(:config_value).from(table(:config)).where(:config_name => 'upload_path')).as(:attachment_path)
+        select_config_value('version').as(:phpbb_version),
+        select_config_value('avatar_gallery_path').as(:avatar_gallery_path),
+        select_config_value('avatar_path').as(:avatar_path),
+        select_config_value('avatar_salt').as(:avatar_salt),
+        select_config_value('smilies_path').as(:smilies_path),
+        select_config_value('upload_path').as(:attachment_path)
       ).first
     end
 
@@ -261,21 +267,31 @@ module ImportScripts::PhpBB3
     # this removes duplicate messages, converts the to_address to a number
     # and stores the message_subject in lowercase and without the prefix "Re: "
     def fill_temp_import_message_table
-      recipient_id = Sequel.case([[{m__root_level: 0, position(:m__to_address, ':') => 0},
-                                   Sequel.cast(substring(:m__to_address, 3), Integer)]], nil).as(:recipient_id)
+      # CASE WHEN m.root_level = 0 AND INSTR(m.to_address, ':') = 0 THEN
+      # CAST(SUBSTRING(m.to_address, 3) AS SIGNED INTEGER)
+      # ELSE NULL END AS recipient_id
+      is_root_message_with_one_recipient = {m__root_level: 0, position(:m__to_address, ':') => 0}
+      extract_user_id = Sequel.cast(substring(:m__to_address, 3), Integer)
+      recipient_id = Sequel.case([[is_root_message_with_one_recipient, extract_user_id]], nil).as(:recipient_id)
 
-      normalized_subject = Sequel.function(:lower, Sequel.case([[Sequel.like(:m__message_subject, 'Re: %'),
-                                                                 substring(:m__message_subject, 5)]],
-                                                               :m__message_subject)).as(:normalized_subject)
+      # LOWER(CASE WHEN m.message_subject LIKE 'Re: %' THEN
+      # SUBSTRING(m.message_subject, 5)
+      # ELSE m.message_subject END) AS normalized_subject
+      subject_starts_with_re = Sequel.like(:m__message_subject, 'Re: %')
+      subject = Sequel.case([[subject_starts_with_re, substring(:m__message_subject, 5)]], :m__message_subject)
+      normalized_subject = Sequel.function(:lower, subject).as(:normalized_subject)
+
+      is_duplicate_message = Sequel.virtual_row { (x__msg_id < m__msg_id) & (x__root_level =~ m__root_level) &
+        (x__author_id =~ m__author_id) & (x__to_address =~ m__to_address) & (x__message_time =~ m__message_time) }
 
       subquery = @database
                    .select(:m__msg_id, :m__root_level, recipient_id, normalized_subject)
                    .from(table(:privmsgs, :m))
-                   .where(@database.select(1)
+                   .where(@database
+                            .select(1)
                             .from(table(:privmsgs, :x))
-                            .where { (x__msg_id < m__msg_id) & (x__root_level =~ m__root_level) &
-                            (x__author_id =~ m__author_id) & (x__to_address =~ m__to_address) &
-                            (x__message_time =~ m__message_time) }.exists).invert
+                            .where(is_duplicate_message)
+                            .exists).invert
 
       @database[table(:import_privmsgs_temp)]
         .insert([:msg_id, :root_msg_id, :recipient_id, :normalized_subject], subquery)
@@ -295,23 +311,37 @@ module ImportScripts::PhpBB3
     # this tries to calculate the actual root_level (= msg_id of the first message in a
     # private conversation) based on subject, time, author and recipient
     def fill_import_message_table
-      msg_id_subquery = @database.select(:a__msg_id)
+      is_same_conversation = Sequel.virtual_row {
+        (((a__author_id =~ m__author_id) & (b__recipient_id =~ i__recipient_id)) |
+          ((a__author_id =~ i__recipient_id) & (b__recipient_id =~ m__author_id))) &
+          (b__normalized_subject =~ i__normalized_subject) &
+          (a__msg_id !~ m__msg_id) &
+          (a__message_time < m__message_time)
+      }
+      msg_id_subquery = @database
+                          .select(:a__msg_id)
                           .from(table(:privmsgs, :a))
                           .join(table(:import_privmsgs_temp, :b), [:msg_id])
-                          .where { (((a__author_id =~ m__author_id) & (b__recipient_id =~ i__recipient_id)) | ((a__author_id =~ i__recipient_id) & (b__recipient_id =~ m__author_id))) &
-        (b__normalized_subject =~ i__normalized_subject) & (a__msg_id !~ m__msg_id) & (a__message_time < m__message_time) }
+                          .where(is_same_conversation)
                           .order(:a__message_time)
                           .limit(1)
 
       root_msg_id = Sequel.case({0 => Sequel.function(:coalesce, msg_id_subquery, 0)}, 0, :i__root_msg_id).as(:root_msg_id)
 
-
-      subquery = @database.select(:m__msg_id, root_msg_id)
+      subquery = @database
+                   .select(:m__msg_id, root_msg_id)
                    .from(table(:privmsgs, :m))
                    .join(table(:import_privmsgs_temp, :i), [:msg_id])
 
       @database[table(:import_privmsgs)]
         .insert([:msg_id, :root_msg_id], subquery)
+    end
+
+    def select_config_value(config_name)
+      @database
+        .select(:config_value)
+        .from(table(:config))
+        .where(:config_name => config_name)
     end
   end
 end
--- script/import_scripts/phpbb3/database/database_3_1.rb
+++ script/import_scripts/phpbb3/database/database_3_1.rb
@@ -2,9 +2,12 @@ require_relative 'database_3_0'
 require_relative '../support/constants/constants'
 
 module ImportScripts::PhpBB3
+  # noinspection RubyResolve
   class Database_3_1 < Database_3_0
     def fetch_users(offset)
       unix_timestamp = Time.now.to_i
+      banlist_join_condition = Sequel.virtual_row { (u__user_id =~ b__ban_userid) & (b__ban_exclude =~ 0) &
+        ((b__ban_end =~ 0) | (b__ban_end >= unix_timestamp)) }
 
       @database
         .select(:u__user_id, :u__user_email, :u__username, :u__user_password, :u__user_regdate, :u__user_lastvisit,
@@ -14,8 +17,7 @@ module ImportScripts::PhpBB3
         .from(table(:users, :u))
         .join(table(:profile_fields_data, :f), [:user_id])
         .join(table(:groups, :g), [:group_id])
-        .left_outer_join(table(:banlist, :b), Sequel.virtual_row { (u__user_id =~ b__ban_userid) & (b__ban_exclude =~ 0) &
-                                              ((b__ban_end =~ 0) | (b__ban_end >= unix_timestamp)) })
+        .left_join(table(:banlist, :b), banlist_join_condition)
         .where { (u__user_type !~ Constants::USER_TYPE_IGNORE) }
         .order(:u__user_id)
         .limit(@batch_size)
