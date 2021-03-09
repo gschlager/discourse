270c5203bb39014347aedd2eecb5e98b1bcdb037
Gerhard Schlager <gerhard.schlager@discourse.org>
2021-03-10 00:39:44 +0100

    WIP

--- lib/backup_restore_new/backup/upload_backuper.rb
+++ lib/backup_restore_new/backup/upload_backuper.rb
@@ -4,8 +4,12 @@ require 'mini_tarball'
 
 module BackupRestoreNew
   class UploadBackuper
-    def initialize(progress_logger)
+    attr_reader :errors
+
+    def initialize(tmp_directory, progress_logger)
+      @tmp_directory = tmp_directory
       @progress_logger = progress_logger
+      @errors = []
     end
 
     def compress_uploads(output_stream)
@@ -21,18 +25,55 @@ module BackupRestoreNew
 
     def add_uploaded_files(tar_writer)
       Upload.by_users.find_each do |upload|
-        relative_path = Discourse.store.get_path_for_upload(upload)
-        absolute_path = File.join(Rails.root, "public", relative_path)
-        relative_path.delete_prefix!("/")
+        paths_of(upload) do |relative_path, absolute_path|
+          next if absolute_path.blank?
 
-        if File.exist?(absolute_path)
-          tar_writer.add_file(name: relative_path, source_file_path: absolute_path)
-        else
-          # puts "Missing file: #{absolute_path}"
+          if File.exist?(absolute_path)
+            tar_writer.add_file(name: relative_path, source_file_path: absolute_path)
+          else
+            log_error("Failed to locate file with upload ID #{upload.id}")
+          end
         end
 
         @progress_logger.increment
       end
+    end
+
+    def paths_of(upload)
+      is_local_upload = upload.local?
+      store = store_for(is_local_upload)
+
+      relative_path = store.get_path_for_upload(upload)
+      relative_path.delete_prefix!("/")
+
+      if is_local_upload
+        absolute_path = File.join(Rails.root, "public", relative_path)
+      else
+        absolute_path = File.join(@tmp_directory, upload.sha1)
+
+        begin
+          store.download_file(upload, filename)
+        rescue StandardError => ex
+          absolute_path = nil
+          log_error("Failed to download file with upload ID #{upload.id} from S3", ex)
+        end
+      end
+
+      yield(relative_path, absolute_path)
+
+      FileUtils.rm_f(absolute_path) if !is_local_upload && absolute_path
+    end
+
+    def store_for(is_local_upload)
+      if is_local_upload
+        @local_store ||= FileStore::LocalStore.new
+      else
+        @s3_store ||= FileStore::S3Store.new
+      end
+    end
+
+    def log_error(message, ex = nil)
+      @errors << { message: message, ex: ex }
     end
   end
 end
--- lib/backup_restore_new/backuper.rb
+++ lib/backup_restore_new/backuper.rb
@@ -44,20 +44,22 @@ module BackupRestoreNew
     def initialize_backup
       log_task("Initializing backup") do
         @success = false
-        @backup_path = calculate_backup_path
-      end
-    end
 
-    def calculate_backup_path
-      filename = @filename_override || begin
-        parameterized_title = SiteSetting.title.parameterize.presence || "discourse"
         timestamp = Time.now.utc.strftime("%Y-%m-%d-%H%M%S")
-        "#{parameterized_title}-#{timestamp}"
-      end
+        current_db = RailsMultisite::ConnectionManagement.current_db
+        archive_directory = BackupRestore::LocalBackupStore.base_directory(db: current_db)
 
-      current_db = RailsMultisite::ConnectionManagement.current_db
-      archive_directory = BackupRestore::LocalBackupStore.base_directory(db: current_db)
-      File.join(archive_directory, "#{filename}.tar")
+        filename = @filename_override || begin
+          parameterized_title = SiteSetting.title.parameterize.presence || "discourse"
+          "#{parameterized_title}-#{timestamp}"
+        end
+
+        @backup_path = File.join(archive_directory, "#{filename}.tar")
+        @tmp_directory = File.join(Rails.root, "tmp", "backups", current_db, timestamp)
+
+        FileUtils.mkdir_p(archive_directory)
+        FileUtils.mkdir_p(@tmp_directory)
+      end
     end
 
     def create_backup
@@ -79,7 +81,7 @@ module BackupRestoreNew
     def add_uploads(tar_writer)
       log_task("Adding uploads", with_progress: true) do |progress_logger|
         tar_writer.add_file_from_stream(name: BackupRestore::UPLOADS_FILE, **tar_file_attributes) do |output_stream|
-          backuper = UploadBackuper.new(progress_logger)
+          backuper = UploadBackuper.new(@tmp_directory, progress_logger)
           backuper.compress_uploads(output_stream)
         end
       end
